e1afd0f83304ee6de8f78a52272ca910af3aee7f
Jahnavi <LENOVO@DESKTOP-OU99U2Q>
2026-01-14 16:35:13 +0530

    added a mongo

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,28 +1,26 @@
-// lib/mongodb.ts - MongoDB connection helper
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_DB_NAME;
 
-if (!uri) {
-    throw new Error('MONGODB_URI is not defined');
-}
-if (!dbName) {
-    throw new Error('MONGODB_DB_NAME is not defined');
-}
+if (!uri) throw new Error('MONGODB_URI is not defined');
+if (!dbName) throw new Error('MONGODB_DB_NAME is not defined');
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
+// Extend global type for TypeScript
+declare global {
+    var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 if (process.env.NODE_ENV === 'development') {
-    // In development mode, use a global variable so that the value is preserved across module reloads
     if (!global._mongoClientPromise) {
         client = new MongoClient(uri);
         global._mongoClientPromise = client.connect();
     }
-    clientPromise = global._mongoClientPromise;
+    clientPromise = global._mongoClientPromise!;
 } else {
-    // In production mode, it's best to not use a global variable
     client = new MongoClient(uri);
     clientPromise = client.connect();
 }
